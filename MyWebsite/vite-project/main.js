0a2b7fc7d685b1e8f8091dbaa27a64793d21172d
aang88 <angaaron02@gmail.com>
2023-01-16 15:47:37 -0500

    first commit

---
+++ MyWebsite/vite-project/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+
+
+
+
+// document.querySelector('#app').innerHTML = `
+// `
+// setupCounter(document.querySelector('#counter'))
